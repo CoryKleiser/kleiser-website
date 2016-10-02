b2a5ba54ba10c5ac20caab53bd0b9c9ad256ba22
Cory Kleiser <kleisecj@mail.uc.edu>
2016-10-02 08:56:41 -0400

    added tadoo services

---
+++ js/KleiserWebAppServices.tadooServices.js
@@ -0,0 +1,136 @@
+'use strict';
+(function() {
+
+
+    var tadooServices = angular.module(`tadooServices`, []);
+
+
+        //This service locates the user
+    tadooServices.service(`locate`, function(){
+        const locate = this;
+        locate.map;
+        locate.userLocation;
+
+        locate.findUser = function(){
+            return new Promise(function (resolve, reject) {
+
+
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    locate.userLocation = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+                });
+                locate.map = new google.maps.Map($('#mapContainer'), {
+                    center: locate.userLocation,
+                    zoom: 14,
+                });
+
+                return (locate.userLocation);
+            });
+        };
+    });
+
+
+            //service to find places
+    tadooServices.service(`places`, function($http, $location, locate) {
+
+        const places = this;
+
+        // function to find places
+        places.findPlaces = function(request){
+            return new Promise(function(resolve, reject) {
+
+                const service = new google.maps.places.PlacesService(locate.map);
+
+                //instantiates found array
+                places.found = [];
+
+                //performs search and filters content in for loop
+                service.nearbySearch(request, function(results, status) {
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+                        //: cycle through place results and filter needed information
+                        //value to set spot in array
+                        var j = 0;
+                        for (var i = 0; i < results.length; i++) {
+                            var place = results[i];
+
+                            //set results
+                            const genInfo = {
+                                index: j,
+                                id: place.place_id,
+                                name: place.name,
+                                googleRating: place.rating,
+                                photo: place.icon,
+                                //TODO: implement open now status to show under info
+                                //openNow: place.opening_hours.open_now,
+                                address: place.vicinity,
+                                categories: place.types
+                            };
+
+                            if (place.rating == undefined){
+                                genInfo.googleRating = `N/A`;
+                            }
+
+                            //:  filter logic to improve places found
+                            if (place.rating >= 3.8 | place.types.includes(`gas_station`)) {
+
+                                places.found.push(genInfo);
+                                j++;
+
+                            }
+
+                        }
+
+                        //test out places
+                        resolve(places.found);
+                        reject($location.hash(`#/categories/`));
+                    }
+                    //: Handle Error
+                    else {
+                        console.log(status);
+                        alert(`Unable find your location!`);
+                    }
+                });
+
+
+                //TODO: find details on specified place
+                places.findDetails = function (id) {
+
+                    function requestListener(){
+                        console.log(this.responseText);
+                    }
+
+                    var xhr = new XMLHttpRequest();
+                    console.log(xhr);
+                    xhr.withCredentials = true;
+                    xhr.addEventListener("load", requestListener);
+                    xhr.open('GET', 'https://maps.googleapis.com/maps/api/place/details/json?placeid='+id+'&key=AIzaSyAJqkmPJs8LI71k7dSWc-KuYSi5zEoNpwk');
+                    xhr.send();
+
+
+                    // return $http({
+                    //     method: 'GET',
+                    //     url: 'https://maps.googleapis.com/maps/api/place/details/json?placeid='+id+'&key=AIzaSyAJqkmPJs8LI71k7dSWc-KuYSi5zEoNpwk'
+                    // }).
+                    // success(function(data, status) {
+                    //     console.log(status);
+                    //     var data = data;
+                    //     alert(JSON.stringify(data));
+                    // }).
+                    // error(function(data, status) {
+                    //     var data = data || "Request failed";
+                    //     var status = status;
+                    //     alert(data+status);
+                    // });
+
+                    // .then(function callbackSuccess (data) {
+                    //     console.log(data);
+                    // });
+                }
+            });
+        };
+    });
+})();
+
